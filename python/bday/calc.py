7436c278d46e635391d1eee691c6e08ed6cb01df
A13c <52516208+A13ccc@users.noreply.github.com>
2024-04-30 11:55:52 -0400

    Add files via upload

---
+++ python/bday/calc.py
@@ -0,0 +1,18 @@
+def add(num1, num2):
+  return num2 + num2
+  
+  
+def sub(num1, num2):
+  return num1 - num2
+  
+  
+def mult(num1, num2):
+  return num1 * num2
+  
+  
+def divide(num1, num2):
+  return num1 / num2
+  
+  
+def exp(num1, num2):
+  return num1 ** num2
